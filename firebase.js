6d518f1a2da4f82a273e2b235125765e2e074f69
Michal Kaliszewski <michal.kaliszewski2@gmail.com>
2022-07-18 21:40:21 +0200

    init

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4VQg13ZSkBjpU3NRABW3qffDdHCr55w4",
+  authDomain: "scan-4ffda.firebaseapp.com",
+  projectId: "scan-4ffda",
+  storageBucket: "scan-4ffda.appspot.com",
+  messagingSenderId: "914981864332",
+  appId: "1:914981864332:web:1f1829730e9b703593cca9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
